352e20f148c0572e5a2fbbb851ce14ca11da3e6f
Gpaqsa <giorgi.paksashvili.2@btu.edu.ge>
2023-10-19 15:54:17 +0400

    Add new data

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,25 +4,36 @@
 
 
 @section('content')
-    <h1>Quiz App</h1>
-    <ul class="quiz-list-group">
-        @foreach ($quizzes as $quiz)
-            <li class="quiz-list-group-item">
-                {{ $quiz['name'] }}
-                <span class="float-right">{{ $quiz['status'] }}</span>
-            </li>
-        @endforeach
-    </ul>
-
+<h1>Quiz App</h1>
+<ul class="quiz-list-group">
+    @foreach ($quizzes as $quiz)
+    <li class="quiz-list-group-item">
+        {{ $quiz['name'] }}
+        <span class="float-right"> Status: {{ $quiz['status'] }}</span>
+        <img src="{{ $quiz['image'] }}" alt="image" class="img-fluid img-thumbnail rounded" />
+    </li>
+    @endforeach
+</ul>
+<footer>
     <div class="mt-4">
         <h2>Subscribe to Us</h2>
         <form action="{{ route('subscribe') }}" method="POST">
             @csrf
             <div>
                 <label for="eamil">Email:</label>
-                <input type="Email" name="email", id="email", class="form-control", required/>
+                <input type="Email" name="email" , id="email" , class="form-control" , required />
             </div>
             <button type="submit" class="btn btn-primary">Subscribe</button>
         </form>
     </div>
+</footer>
+
 @endsection
+
+@push('css')
+<link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+@endpush
+
+@push('js')
+<script src="{{ asset(' js/app.js') }}"></script>
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -51,15 +51,15 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('/', function() {
+Route::get('/', function () {
     $quizzes = [
-        ['name' => 'What does FIFA stand for in English?', 'status' => "active"],
-        ['name' => 'Who is the world’s highest-paid athlete in 2021?', 'status' => "active"],
-        ['name' => 'What was the first name of Argentinian soccer star Maradona?', 'status' => "inactive"],
+        ['name' => 'What does FIFA stand for in English?', 'image' => 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/05/28/12/fifa.jpg?quality=75&width=1200&auto=webp', 'status' => "active"],
+        ['name' => 'Who is the world’s highest-paid athlete in 2021?', 'image' => 'https://www.gyuanyule.com/png/13170509-forbes-highest-paid-athletes-in-the-world-2021_cover_1500x1000.png',  'status' => "active"],
+        ['name' => 'What was the first name of Argentinian soccer star Maradona?', 'image' => 'https://cdn.britannica.com/76/124976-050-E03E50CE/Diego-Maradona-1986.jpg', 'status' => "inactive"],
     ];
     return view('main', ['quizzes' => $quizzes]);
-})-> name('main');
+})->name('main');
 
-Route::post('/subscribe', function(){
+Route::post('/subscribe', function () {
     return redirect()->route('main')->with('message', "Success");
 })->name('subscribe');
